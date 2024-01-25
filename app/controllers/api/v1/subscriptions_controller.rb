f8e27731afce8a89704f4277ab01a91ab86df2ba
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-25 13:27:50 -0700

    start building customer show page

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  def index
-    
+  def show
+
   end
 end
--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 describe 'customer API' do
   it "has customer information" do
     create_list(:customer, 3)
-    get '/api/v1/customers'
+    get api_v1_customer_path
     expect(response).to be_successful
+    customers = JSON.parse(response.body)
+    require 'pry'; binding.pry
   end
 
 end
