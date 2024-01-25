dae44ee59334b6d12c5a32fee7b34363960d495e
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-25 11:31:38 -0700

    setup factory for customers

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :subscription do
-    
+    title { Faker::Subscription.plan }
+    price { Faker::Number.number(digits: 1) }
+    status { Faker::Subscription.status }
+    frequency { Faker::Subscription.payment_term }
   end
 end
--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'customer API' do
   it "has customer information" do
+    create_list(:customer, 3)
     get '/api/v1/customers'
     expect(response).to be_successful
   end
